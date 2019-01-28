99ecc903e6dd8bc59aa6bcde3923d4fd869f5e2f
Luuk W. (Twinone) <twinonedevs@gmail.com>
2019-01-29 00:18:32 +0100

    Cleanup

--- App/app/src/main/java/com/jediupc/helloandroid/ContentActivity.java
+++ App/app/src/main/java/com/jediupc/helloandroid/ContentActivity.java
@@ -2,21 +2,15 @@ package com.jediupc.helloandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class ContentActivity extends AppCompatActivity {
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
-
 
     }
 }
